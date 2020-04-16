132c23605186a3219c8594fedb4b66ce2c34177f
Krannich479 <krannich479@googlemail.com>
2020-04-16 10:59:30 +0200

    major rework of the setcover class

--- src/Setcover.h
+++ src/Setcover.h
@@ -1,111 +1,152 @@
-/*!
-* \file    src/setcover.h
-* \brief   This file separates the setcover object from the main code
-*
-*/
+/**
+ *  @file    src/setcover.h
+ *  @brief   This file implements the setcover class
+ */
 #ifndef SETCOVER_
 #define SETCOVER_
 
 #include "UnitigExtension.h"
 #include <unordered_set>
-#include <vector>
+#include <unordered_map>
 
-#include <seqan/basic.h>
-
-// NOTE: delete at release
-#include "prettyprint.h"
+#include "prettyprint.h"        // TODO: delete at release
 
 
 
-/*!
-* \class    Setcover
-* \brief    This class implements a setcover for the ccDBG's unitigs.
-*/
-template <typename TSetcover     = std::unordered_set<unsigned>,
-          typename TCurrentPath  = std::unordered_map<unsigned, unsigned> >
+/**
+ *  @class    Setcover
+ *  @brief    This class implements a setcover
+ */
 class Setcover{
+
+    typedef std::unordered_map<unsigned, size_t> path_t;
+    typedef std::unordered_set<unsigned> setcover_t;
+
+    size_t _min_kmer_contribution;
+
+    setcover_t _setcover;
+
+    path_t _current_path;
+
 public:
-    // Constructors
-    Setcover() : t_(255) {}
-    Setcover(unsigned t) : t_(t) {}
+    /**
+     *          Constructors
+     *  @brief  The Setcover::Setcover determines the smallest possible contig
+     *          the PopIns2 merge module can return.
+     *  @param  threshold is the minimum amount of novel kmers a path has to
+     *          contribute to the setcover to be included.
+     */
+    Setcover() : _min_kmer_contribution(62) {}      // 62 kmers = 125 bases = 2k-1 (for a default k=63)
+    Setcover(size_t threshold) : _min_kmer_contribution(threshold) {}
+
 
     /**
-     *          Checks if the cumulative sum of unseen kmers of the current path p is LEQ a threshold.
-     * @return  true if cumsum(p) is larger than or equal threshold t_
+     *          Function to add an element to the setcover
+     *  @param  id is a unitig id
+     *  @param  nb_kmers is the amount of kmers of a unitig
      */
-    bool hasMinContribution(bool verbose = false) const{
-        unsigned novel_kmers = 0;
-        auto cit = current_path_.cbegin();
-        while (cit != current_path_.cend()){
-            if (!contains(cit->first)){
-                novel_kmers += cit->second;
-                if (verbose) cout << cit->first << " is novel" << endl;
-            }
-            else{
-                if (verbose) cout << cit->first << " is not novel" << endl;
-            }
-            if (novel_kmers >= t_){
-                if (verbose) cout << "Found enough novel kmers" << endl;
-                return true;
-            }
-            ++cit;
-        }
-        if (verbose) cout << "Found not enough novel kmers" << endl;
-        return false;
-    }
+    void add(unsigned id, size_t nb_kmers){_current_path.insert(std::pair<unsigned, size_t>(id, nb_kmers));}
 
-    // add/delete/clear functions for the current path container
-    void add(unsigned err){(void)err; SEQAN_THROW(seqan::RuntimeError("Argument error in sc.add() "));}
-    void add(unsigned id, unsigned nb_kmers){current_path_.insert(std::pair<unsigned, unsigned>(id, nb_kmers));}
+
+    /**
+     *          Function to add an element to the setcover
+     *  @param  ucm is a unitig
+     */
     void add(const UnitigColorMap<UnitigExtension> &ucm){
-        DataAccessor<UnitigExtension>* ucm_da = ucm.getData();
-        UnitigExtension* ucm_ue = ucm_da->getData(ucm);
-        add(ucm_ue->getID(), ucm.len);
-    }
-    void del(unsigned id){current_path_.erase(id);}
-    void del(const UnitigColorMap<UnitigExtension> &ucm){
-        DataAccessor<UnitigExtension>* ucm_da = ucm.getData();
-        UnitigExtension* ucm_ue = ucm_da->getData(ucm);
-        del(ucm_ue->getID());
-    }
-    void clear(){current_path_.clear();}
 
-    bool empty(){return c_.empty();}
+        DataAccessor<UnitigExtension>* da = ucm.getData();
+        UnitigExtension* ue = da->getData(ucm);
 
-    // Unify the the elements of the current path with the set cover
-    void incorporate(bool verbose = false){
-        typename TCurrentPath::const_iterator cit = current_path_.cbegin();
-        for ( ; cit != current_path_.cend(); ++cit) c_.insert(cit->first);
-        if (verbose) cout << "Inserted kmers of following novel elements " << endl;
-        if (verbose) print_current();
-        if (verbose) cout << endl;
+        this->add(ue->getID(), ucm.size - (ucm.getGraph()->getK() - 1));
     }
 
-    void write_CSV(const std::string s) const{
-        std::string csv_file_name = s+".csv";
-        ofstream csv_f(csv_file_name);
-        csv_f << "ID,Colour" << endl;
-        for (auto it=c_.cbegin(); it != c_.cend(); ++it) csv_f << *it << ",red" << endl;
-        csv_f.close();
+
+    /**
+     *          Function to test whether a new path should be included into the setcover
+     *  @brief  The function checks whether there is enough novel kmer contribution
+     *          of the vurrent path with respect to the setcover. If true then
+     *          the elements of the current path are flushed into the setcover.
+     *  @return bool; true if path was incorporated into the setcover
+     */
+    bool test(){
+
+        if(!has_min_contribution()){
+
+            _current_path.clear();
+
+            /* DEBUG */ std::cout << "SETCOVER: Path was NOT included."
+
+            return false;
+        }
+
+        for (path_t::const_iterator cit=_current_path.cbegin() ; cit!=_current_path.cend(); ++cit)
+            _setcover.insert(cit->first);
+
+        _current_path.clear();
+
+        /* DEBUG */ std::cout << "SETCOVER: Path was included."
+
+        return true;
     }
 
-    void print_current(){prettyprint::print(current_path_);}
 
-    bool contains(const unsigned id) const{
-        if (c_.find(id)!=c_.end())
-            return true;
-        return false;
+    /**
+     *          This function writes a CSV file of IDs that are included in the setcover
+     *  @param  ofile_prefix is the prefix of file to be written
+     */
+    void write(const std::string ofile_prefix) const{
+
+        std::string fname = ofile_prefix+".setcover.csv";
+        ofstream ofile(fname);
+
+        if (ofile.is_open()){
+
+            ofile << "ID,Colour" << endl;
+
+            for (auto cit=_setcover.cbegin(); cit != _setcover.cend(); ++cit)
+                ofile << *cit << ",red" << endl;
+
+            ofile.close();
+        }
+        else cerr << "[popins2 merge] [Setcover::write]: Unable to open output file.";
     }
 
 private:
-    // minimum contribution of a path, checked in Setcover::check()
-    unsigned t_;
 
-    // set cover
-    TSetcover c_;
+    void print_current_path() const{prettyprint::print(_current_path);}
 
-    // container for the curent path
-    TCurrentPath current_path_;
+
+    /**
+     *          This functions checks if the cumulative sum of kmers that are
+     *          not in the setcover yet surpass (GEQ) the minimum threshold.
+     *  @return true if path has enough novel contribution
+     */
+    bool has_min_contribution() const{
+
+        size_t novel_kmers = 0;
+
+        path_t::const_iterator cit = _current_path.cbegin();
+        while (cit != _current_path.cend()){
+
+            if (!contains(cit->first))              // first  = ID
+                novel_kmers += cit->second;         // second = #kmers of the unitig with ID
+
+            if (novel_kmers >= _min_kmer_contribution)
+                return true;
+
+            ++cit;
+        }
+
+        return false;
+    }
+
+
+    /**
+     *          This function checks if a given id is in the setcover
+     *  @param  id is the id to search for in the setcover
+     *  @return true if id is already in setcover
+     */
+    bool contains(const unsigned id) const{return (_setcover.find(id) != c_.cend()) ? true : false;}
 
 };
 
