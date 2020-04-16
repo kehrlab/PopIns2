8b3954f21155fe809525760116a012d2ce03d9b7
Krannich479 <krannich479@googlemail.com>
2020-04-16 12:01:29 +0200

    split setcover class in .h and .cpp

---
+++ src/Setcover.cpp
@@ -0,0 +1,74 @@
+#include "Setcover.h"
+
+
+
+void Setcover::add(const UnitigColorMap<UnitigExtension> &ucm){
+
+    DataAccessor<UnitigExtension>* da = ucm.getData();
+    UnitigExtension* ue = da->getData(ucm);
+
+    this->add(ue->getID(), ucm.size - (ucm.getGraph()->getK() - 1));
+}
+
+
+bool Setcover::test(){
+
+    if(!has_min_contribution()){
+
+        _current_path.clear();
+
+        /* DEBUG */ std::cout << "SETCOVER: Path was NOT included." << std::endl;
+
+        return false;
+    }
+
+    for (path_t::const_iterator cit=_current_path.cbegin() ; cit!=_current_path.cend(); ++cit)
+        _setcover.insert(cit->first);
+
+    _current_path.clear();
+
+    /* DEBUG */ std::cout << "SETCOVER: Path was included." << std::endl;
+
+    return true;
+}
+
+
+void Setcover::write(const std::string ofile_prefix) const{
+
+    std::string fname = ofile_prefix+".setcover.csv";
+    ofstream ofile(fname);
+
+    if (ofile.is_open()){
+
+        ofile << "ID,Colour" << endl;
+
+        for (auto cit=_setcover.cbegin(); cit != _setcover.cend(); ++cit)
+            ofile << *cit << ",red" << endl;
+
+        ofile.close();
+    }
+    else cerr << "[popins2 merge] [Setcover::write]: Unable to open output file.";
+}
+
+
+inline bool Setcover::has_min_contribution() const{
+
+    size_t novel_kmers = 0;
+
+    path_t::const_iterator cit = _current_path.cbegin();
+    while (cit != _current_path.cend()){
+
+        if (!contains(cit->first))              // first  = ID
+            novel_kmers += cit->second;         // second = #kmers of the unitig with ID
+
+        if (novel_kmers >= _min_kmer_contribution)
+            return true;
+
+        ++cit;
+    }
+
+    return false;
+}
+
+
+// EOF
--- src/Setcover.h
+++ src/Setcover.h
@@ -52,13 +52,7 @@ public:
      *          Function to add an element to the setcover
      *  @param  ucm is a unitig
      */
-    void add(const UnitigColorMap<UnitigExtension> &ucm){
-
-        DataAccessor<UnitigExtension>* da = ucm.getData();
-        UnitigExtension* ue = da->getData(ucm);
-
-        this->add(ue->getID(), ucm.size - (ucm.getGraph()->getK() - 1));
-    }
+    void add(const UnitigColorMap<UnitigExtension> &ucm);
 
 
     /**
@@ -68,48 +62,14 @@ public:
      *          the elements of the current path are flushed into the setcover.
      *  @return bool; true if path was incorporated into the setcover
      */
-    bool test(){
-
-        if(!has_min_contribution()){
-
-            _current_path.clear();
-
-            /* DEBUG */ std::cout << "SETCOVER: Path was NOT included."
-
-            return false;
-        }
-
-        for (path_t::const_iterator cit=_current_path.cbegin() ; cit!=_current_path.cend(); ++cit)
-            _setcover.insert(cit->first);
-
-        _current_path.clear();
-
-        /* DEBUG */ std::cout << "SETCOVER: Path was included."
-
-        return true;
-    }
+    bool test();
 
 
     /**
      *          This function writes a CSV file of IDs that are included in the setcover
      *  @param  ofile_prefix is the prefix of file to be written
      */
-    void write(const std::string ofile_prefix) const{
-
-        std::string fname = ofile_prefix+".setcover.csv";
-        ofstream ofile(fname);
-
-        if (ofile.is_open()){
-
-            ofile << "ID,Colour" << endl;
-
-            for (auto cit=_setcover.cbegin(); cit != _setcover.cend(); ++cit)
-                ofile << *cit << ",red" << endl;
-
-            ofile.close();
-        }
-        else cerr << "[popins2 merge] [Setcover::write]: Unable to open output file.";
-    }
+    void write(const std::string ofile_prefix) const;
 
 private:
 
@@ -121,24 +81,7 @@ private:
      *          not in the setcover yet surpass (GEQ) the minimum threshold.
      *  @return true if path has enough novel contribution
      */
-    bool has_min_contribution() const{
-
-        size_t novel_kmers = 0;
-
-        path_t::const_iterator cit = _current_path.cbegin();
-        while (cit != _current_path.cend()){
-
-            if (!contains(cit->first))              // first  = ID
-                novel_kmers += cit->second;         // second = #kmers of the unitig with ID
-
-            if (novel_kmers >= _min_kmer_contribution)
-                return true;
-
-            ++cit;
-        }
-
-        return false;
-    }
+    bool has_min_contribution() const;
 
 
     /**
@@ -146,7 +89,7 @@ private:
      *  @param  id is the id to search for in the setcover
      *  @return true if id is already in setcover
      */
-    bool contains(const unsigned id) const{return (_setcover.find(id) != c_.cend()) ? true : false;}
+    bool contains(const unsigned id) const{return (_setcover.find(id) != _setcover.cend()) ? true : false;}
 
 };
 
